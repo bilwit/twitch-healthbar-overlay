f1c30eff20b7890be50d1fbdae8b60adb4519408
leoo <leoo@synology.com>
2023-09-26 17:09:14 -0700

    implements express hosting vite dist

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import App from './App';
+import './index.css';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// eslint-disable-next-line
+const container: any = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
+);
