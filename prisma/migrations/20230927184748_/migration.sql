26c30f1370178fb2b681e570cb5365ac6b20ad92
leoo <leoo@synology.com>
2023-09-27 11:48:13 -0700

    adds Refresh Token schema

---
+++ prisma/migrations/20230927184748_/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "RefreshToken" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "RefreshToken_pkey" PRIMARY KEY ("id")
+);
