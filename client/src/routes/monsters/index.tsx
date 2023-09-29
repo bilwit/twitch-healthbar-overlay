da1927c5f5f32afa28c6322f240cc099556b3d0d
leoo <leoo@synology.com>
2023-09-29 13:16:07 -0700

    adds alerts for monsters route

--- client/src/routes/monsters/index.tsx
+++ client/src/routes/monsters/index.tsx
@@ -1,24 +1,61 @@
 
 import { 
+  Alert,
   SimpleGrid,
 } from '@mantine/core';
 import useGetMonsters from './useGetMonsters';
+import { BiError, BiInfoCircle } from 'react-icons/bi';
+import classes from '../../css/Nav.module.css';
 
 function Monsters() {
-  const { monsters } = useGetMonsters();
+  const { monsters, error } = useGetMonsters();
   
   return (
-    <SimpleGrid
-      cols={{ base: 1, sm: 2, lg: 5 }}
-      spacing={{ base: 10, sm: 'xl' }}
-      verticalSpacing={{ base: 'md', sm: 'xl' }}
-    >
-    {monsters && monsters.length > 0 && monsters.map((monster) => (
-      <div key={monster.id}>
-        {monster.name}
-      </div>
-    ))}
-    </SimpleGrid>
+    <>
+      {error && (
+        <Alert 
+          className={classes['margin-bottom-1']}
+          variant="light" 
+          color="red" 
+          title="Error" 
+          icon={
+            <BiError 
+              size="1rem" 
+              stroke={1.5} 
+            />
+          }
+        >
+          {error}
+        </Alert>
+      )}
+
+      <SimpleGrid
+        cols={{ base: 1, sm: 2, lg: 5 }}
+        spacing={{ base: 10, sm: 'xl' }}
+        verticalSpacing={{ base: 'md', sm: 'xl' }}
+      >
+      {monsters && monsters.length > 0 ? monsters.map((monster) => (
+        <div key={monster.id}>
+          {monster.name}
+        </div>
+      )) : (
+        <Alert 
+          className={classes['margin-bottom-1']}
+          variant="light" 
+          color="indigo" 
+          title="Note" 
+          icon={
+            <BiInfoCircle 
+              size="1rem" 
+              stroke={1.5} 
+            />
+          }
+        >
+          No monsters added yet
+        </Alert>
+      )}
+      </SimpleGrid>
+    </>
   );
 }
 
