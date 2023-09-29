67707d72676a07c684e3a34132b3616ee9d23db9
leoo <leoo@synology.com>
2023-09-29 15:43:31 -0700

    implements setting form fields

--- client/src/routes/settings/index.tsx
+++ client/src/routes/settings/index.tsx
@@ -1,8 +1,15 @@
 
 import { 
+  Box,
+  Button,
+  Checkbox,
+  Group,
   Modal,
+  TextInput,
 } from '@mantine/core';
 import { Settings as Interface_Settings } from './useGetSettings';
+import { useForm } from '@mantine/form';
+import classes from '../../css/Nav.module.css';
 
 interface Props {
   settings?: Interface_Settings
@@ -11,6 +18,24 @@ interface Props {
 }
 
 function Settings(props: Props) {
+  const form = useForm({
+    initialValues: {
+      listener_auth_code: '',
+      listener_client_id: '',
+      listener_secret: '',
+      listener_user_name: '',
+      channel_name: '',
+      is_connected: false,
+    },
+
+    validate: {
+      listener_auth_code: (value) => value ? null : 'Required',
+      listener_client_id: (value) => value ? null : 'Required',
+      listener_secret: (value) => value ? null : 'Required',
+      listener_user_name: (value) => value ? null : 'Required',
+      channel_name: (value) => value ? null : 'Required',
+    },
+  });
   
   return (
     <Modal 
@@ -19,7 +44,59 @@ function Settings(props: Props) {
       title="Settings"
       size="xl"
     >
-      {/* Modal content */}
+    <Box mx="auto">
+      <form onSubmit={form.onSubmit((values) => console.log(values))}>
+        <TextInput
+          className={classes['margin-bottom-1']}
+          withAsterisk
+          label="Auth Code"
+          placeholder=""
+          {...form.getInputProps('listener_auth_code')}
+        />
+
+        <TextInput
+          className={classes['margin-bottom-1']}
+          withAsterisk
+          label="Client ID"
+          placeholder=""
+          {...form.getInputProps('listener_client_id')}
+        />
+
+        <TextInput
+          className={classes['margin-bottom-1']}
+          withAsterisk
+          label="Secret"
+          placeholder=""
+          {...form.getInputProps('listener_secret')}
+        />
+
+        <TextInput
+          className={classes['margin-bottom-1']}
+          withAsterisk
+          label="Bot Name"
+          placeholder=""
+          {...form.getInputProps('listener_user_name')}
+        />
+
+        <TextInput
+          className={classes['margin-bottom-1']}
+          withAsterisk
+          label="Channel Name"
+          placeholder=""
+          {...form.getInputProps('channel_name')}
+        />
+
+        <Checkbox
+          mt="md"
+          label="Connect to Twitch"
+          {...form.getInputProps('is_connected', { type: 'checkbox' })}
+        />
+
+        <Group justify="flex-end" mt="md">
+          <Button type="submit">Submit</Button>
+        </Group>
+      </form>
+    </Box>
     </Modal>
   );
 }
