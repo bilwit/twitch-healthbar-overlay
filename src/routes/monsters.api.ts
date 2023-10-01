2f0c53641b4d55d9a83f4365c3aa0d21d3ba4c52
leoo <leoo@synology.com>
2023-10-01 00:25:28 -0700

    implements upload avatar file

--- src/routes/monsters.api.ts
+++ src/routes/monsters.api.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from 'express';
 import { Prisma, PrismaClient } from '@prisma/client';
+import multer from 'multer';
+import fs from 'fs';
+import path from 'path';
 
 const router = express.Router();
 const prisma = new PrismaClient();
@@ -52,6 +55,28 @@ router.get('/:id', async (req: Request, res: Response) => {
   }
 });
 
+const AVATAR_STORAGE_PATH = path.resolve('storage/avatars') + '/';
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    fs.access(AVATAR_STORAGE_PATH, fs.constants.F_OK, (err) => {
+      if (err) {
+        console.log(err);
+        fs.mkdir(AVATAR_STORAGE_PATH, { recursive: false }, (err1) => {
+          console.log(err1);
+          cb(null, AVATAR_STORAGE_PATH);
+        });
+      }
+      cb(null, AVATAR_STORAGE_PATH);
+    });
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '.png');
+  },
+});
+
+const upload = multer({ storage: storage });
+
 router.post('/', async (req: Request, res: Response) => {
   try {
     const monster = await prisma.monster.create({
@@ -87,25 +112,92 @@ router.post('/', async (req: Request, res: Response) => {
   }
 });
 
-router.put('/:id', async (req: Request, res: Response) => {
+const deleteAvatar = (fileName: string): Promise<{msg: string}> => {
+  return new Promise(async (resolve, reject) => {
+    fs.access(
+      AVATAR_STORAGE_PATH + fileName,
+      fs.constants.F_OK,
+      async (err) => {
+        if (err) {
+          reject({ msg: 'File not found' });
+        } else {
+          // delete file from storage
+          fs.unlink(AVATAR_STORAGE_PATH + fileName, (err) => {
+            if (err) {
+              reject({ msg: 'Cannot delete file' });
+            } else {
+              resolve({ msg: 'Avatar deleted' });
+            }
+          });
+        }
+      },
+    );
+  });
+};
+
+interface UpdatedMonsterData {
+  name: string,
+  published: boolean,
+  hp_multiplier: number,
+  trigger_words: string,
+  avatar_url?: string | undefined,
+}
+
+router.put('/:id', upload.single('avatarFile'), async (req: Request, res: Response) => {
   try {
+
+    let deleteOldAvatarPath = '';
+    // delete existing avatar if request indicated change
+    if (JSON.parse(req.body.isAvatarChanged)) {
+      const CheckAvatar = await prisma.monster.findFirst({
+        where: {
+          id: Number(req.params.id),
+        },
+        select: {
+          avatar_url: true,
+        }
+      });
+      if (CheckAvatar?.avatar_url) {
+        deleteOldAvatarPath = CheckAvatar?.avatar_url;
+      }
+    }
+
+    const updatedData: UpdatedMonsterData = {
+      name: JSON.parse(req.body.name),
+      published: JSON.parse(req.body.published),
+      hp_multiplier: JSON.parse(req.body.hp_multiplier),
+      trigger_words: Array.isArray(JSON.parse(req.body.trigger_words)) && JSON.parse(req.body.trigger_words).length > 0 ? JSON.parse(req.body.trigger_words).join(',') : '',
+    }
+
+    if (JSON.parse(req.body.isAvatarChanged)) {
+      updatedData['avatar_url'] = req?.file?.filename || undefined;
+    }
+
     const monster = await prisma.monster.update({
       where: {
         id: Number(req.params.id),
       },
-      data: {
-        name: req.body.name,
-        published: req.body.published,
-        hp_multiplier: req.body.hp_multiplier,
-        trigger_words: req.body.trigger_words.join(','),
-      },
+      data: updatedData,
     });
   
     if (monster && monster?.id > 0) {
+
+      if (deleteOldAvatarPath) {
+        try {
+          const isDeleted = await deleteAvatar(deleteOldAvatarPath);
+          if (isDeleted) {
+            console.log(isDeleted?.msg);
+          }
+        } catch (e) {
+          console.error('Could not delete old avatar');
+        }
+      }
+
       return res.status(200).json({
         success: true,
         data: [monster],
       });
+      
     } else {
       throw true;
     }
@@ -117,6 +209,30 @@ router.put('/:id', async (req: Request, res: Response) => {
       success: false,
     });
   }
+});
+
+router.get('/avatar/:fileName', (req, res) => {
+  const hostFile = (file: string) => {
+    return new Promise(async (resolve, reject) => {
+      // check if file exists
+      fs.access(file, fs.constants.F_OK, async (err) => {
+        if (err) {
+          console.log(err);
+          reject({
+            success: false,
+            msg: 'File does not exist: ' + file,
+          });
+        } else {
+          res.writeHead(200);
+          fs.createReadStream(file).pipe(res);
+          resolve(true);
+        }
+      });
+    });
+  };
+  hostFile(AVATAR_STORAGE_PATH + req.params.fileName).catch((err) => {
+    res.status(500).json(err);
+  });
 });
 
 prisma.$disconnect();
