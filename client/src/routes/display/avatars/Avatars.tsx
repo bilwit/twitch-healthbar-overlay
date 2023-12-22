178fd16589b78e012d89c698c46af40fd9ba14b8
bilwit <lolano07@gmail.com>
2023-12-22 01:00:21 -0800

    implements display stages

--- client/src/routes/display/avatars/Avatars.tsx
+++ client/src/routes/display/avatars/Avatars.tsx
@@ -1,30 +1,24 @@
 import { 
-    MantineProvider,
-    Image,
-  } from '@mantine/core';
-  
-  import { useParams } from "react-router-dom";
-  import useGetData from "../../management/useGetData";
-  // import useWebSocket from '../useWebSocket';
-  
-  function Bars() {
-    const params = useParams();
-    const { isLoading, data: monsters } = useGetData('monsters/base', params?.['*']);
-  
-    // const { data } = useWebSocket(String(monsters?.[0]?.id));
-  
-    // console.log(monsters[0])
-  
-    return (
-      <MantineProvider>
-        {!isLoading && monsters && monsters.length === 1 && (
-          <Image
-            src={window.location.origin + '/api/avatar/' + monsters[0].avatar_url}
-            alt={monsters[0].name + ' Avatar'}
-          />
-        )}
-      </MantineProvider>
-    );
-  }
-  
-  export default Bars;
+  MantineProvider,
+} from '@mantine/core';
+import Item from './Item';
+import { useParams } from "react-router-dom";
+import useGetData from "../../management/useGetData";
+
+function Bars() {
+  const params = useParams();
+  const { isLoading, data: monsters } = useGetData('monsters/base', params?.['*']);
+
+  return (
+    <MantineProvider>
+      {!isLoading && params?.['*'] && monsters && monsters.length === 1 && monsters.map((item) => (
+        <Item 
+          key={item.id}
+          data={item}
+        />
+      ))}
+    </MantineProvider>
+  );
+}
+
+export default Bars;
---
+++ client/src/routes/display/avatars/Item.tsx
@@ -0,0 +1,61 @@
+import { 
+  MantineProvider,
+  Image,
+} from '@mantine/core';
+import useGetData, { Monster, Stage } from "../../management/useGetData";
+import useWebSocket from '../useWebSocket';
+import { useEffect, useState } from 'react';
+
+interface Props {
+  data: Monster,
+}
+
+function Item(props: Props) {
+  const { 
+    isLoading, 
+    data: stages, 
+  } = useGetData('monsters/stages', String(props.data.id));
+  const { data } = useWebSocket(String(props.data.id));
+
+  const [sorted, setSorted] = useState<Stage[]>([]);
+
+  useEffect(() => {
+    if (stages) {
+      setSorted(stages.sort((a, b) => a.hp_value < b.hp_value ? -1 : 1));
+    }
+  }, [stages])
+
+  const displayHealth =  (value: number, maxHealth: number) => {
+    const percentage = value / maxHealth * 100;
+
+    for (const stage of sorted) {
+      if (percentage <= stage.hp_value) {
+        return (
+          <Image
+            src={window.location.origin + '/api/avatar/' + stage.avatar_url}
+            alt={props.data.name + ' Avatar'}
+          />
+        );
+      }
+    }
+
+    return (
+      <Image
+        src={window.location.origin + '/api/avatar/' + props.data.avatar_url}
+        alt={props.data.name + ' Avatar'}
+      />
+    );
+  }
+
+  return (
+    <MantineProvider>
+      {!isLoading && props.data && (
+        <>
+        {displayHealth(data.value, data.maxHealth)}
+        </>
+      )}
+    </MantineProvider>
+  );
+}
+
+export default Item;
