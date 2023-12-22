8e172e41211b373094ea6387cdb091d5e41a50b1
bilwit <lolano07@gmail.com>
2023-12-21 20:21:57 -0800

    fixes lint

--- client/src/routes/management/monsters/stages/Stages.tsx
+++ client/src/routes/management/monsters/stages/Stages.tsx
@@ -1,70 +1,41 @@
 
 import { 
-    Alert,
-    Button,
-    Card,
-    Image,
-    FileInput,
-    Grid,
-    Group,
-    NumberInput,
-    SegmentedControl,
-    Space,
-    TagsInput,
-    TextInput,
-    Text,
-    Overlay,
-    NativeSelect,
-    Stack,
-    Accordion,
-  } from '@mantine/core';
-  import classes from '../../../css/Nav.module.css';
-  import { GiMonsterGrasp } from 'react-icons/gi';
-  import { useForm } from '@mantine/form';
-  import { useState } from 'react';
-  import { theme } from '../../../../theme';
-  import { BiInfoCircle } from 'react-icons/bi';
-  import { BsFillPersonFill } from 'react-icons/bs';
-  import { AiFillDelete } from 'react-icons/ai';
-  import Alerts from '../../Alerts';
-  import CopyURL from '../properties/CopyURL';
+  Alert,
+  Accordion,
+} from '@mantine/core';
+import { BiInfoCircle } from 'react-icons/bi';
 import Item from './Item';
   
-  interface Props {
-    refId?: number,
-  }
-  
-  interface FormDataInterface {
-    healthPercentage: number;
-    avatarFile: File | null;
-  }
-  
-  function Stages(props: Props) {
-    return (
-      <>
-        <Alert 
-          variant="light" 
-          color="indigo" 
-          title="" 
-          mb={"md"}
-          icon={
-            <BiInfoCircle 
-              size="1rem" 
-              stroke={1.5} 
-            />
-          }
-        >
-          <div style={{ display: 'flex' }}>
-            Change the displayed avatar image based on health percentage.
-          </div>
-        </Alert>
+interface Props {
+  refId?: number,
+}
 
-        <Accordion defaultValue="new">
-          <Item refId={props.refId} />
-        </Accordion>
-      </>
-    );
-  }
-  
-  export default Stages;
+function Stages(props: Props) {
+  return (
+    <>
+      <Alert 
+        variant="light" 
+        color="indigo" 
+        title="" 
+        mb={"md"}
+        icon={
+          <BiInfoCircle 
+            size="1rem" 
+            stroke={1.5} 
+          />
+        }
+      >
+        <div style={{ display: 'flex' }}>
+          Change the displayed avatar image based on health percentage.
+        </div>
+      </Alert>
+
+      <Accordion defaultValue="new">
+        <Item refId={props.refId} />
+      </Accordion>
+    </>
+  );
+}
+
+export default Stages;
   
