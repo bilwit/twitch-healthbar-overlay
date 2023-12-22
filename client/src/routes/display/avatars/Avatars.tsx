60caeddcf4a95a0288a2480faf274a8c1f6ac720
bilwit <lolano07@gmail.com>
2023-12-21 17:36:01 -0800

    implements display avatar

--- client/src/routes/display/avatars/Avatars.tsx
+++ client/src/routes/display/avatars/Avatars.tsx
@@ -1,27 +1,30 @@
 import { 
-    MantineProvider
+    MantineProvider,
+    Image,
   } from '@mantine/core';
   
   import { useParams } from "react-router-dom";
-  import useGetData, { Monster } from "../../management/useGetData";
-  import useWebSocket from '../useWebSocket';
+  import useGetData from "../../management/useGetData";
+  // import useWebSocket from '../useWebSocket';
   
   function Bars() {
     const params = useParams();
     const { isLoading, data: monsters } = useGetData('monsters', params?.['*']);
   
-    const { data } = useWebSocket(String(monsters?.[0]?.id));
+    // const { data } = useWebSocket(String(monsters?.[0]?.id));
   
-    console.log(monsters[0])
+    // console.log(monsters[0])
   
     return (
       <MantineProvider>
         {!isLoading && monsters && monsters.length === 1 && (
-          <>hello</>
+          <Image
+            src={window.location.origin + '/api/avatar/' + monsters[0].avatar_url}
+            alt={monsters[0].name + ' Avatar'}
+          />
         )}
       </MantineProvider>
     );
   }
   
   export default Bars;
-  
